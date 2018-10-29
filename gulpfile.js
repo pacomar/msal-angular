fde71bb05f93f85ff714c07b4b49837c6b744f63
Francisco Martinez <francisco.martinez@ciberexperis.es>
2018-10-29 17:21:51 +0100

    Version 1.0.5 release.

--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,82 @@
-var
-    bump = require('gulp-bump'),
-    del = require('del'),
-    exec = require('child_process').exec,
-    gulp = require('gulp'),
-    fs = require('fs');
+var bump = require('gulp-bump'),
+	del = require('del'),
+	exec = require('child_process').exec,
+	gulp = require('gulp'),
+	fs = require('fs');
 
-gulp.task('clean', function () {
-    del(['./dist/*']);
+gulp.task('clean', function() {
+	return del(['./dist/*']);
 });
 
-gulp.task('compile', function (cb) {
-    exec('ngc -p tsconfig-aot.json', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        cb(err);
-    });
+gulp.task('compile', function(cb) {
+	exec('ngc -p tsconfig-aot.json', function(err, stdout, stderr) {
+		console.log(stdout);
+		console.log(stderr);
+		cb(err);
+	});
 });
 
-gulp.task('bump', function () {
-    gulp.src('./package.json')
-        .pipe(bump({
-            type: 'patch'
-        }))
-        .pipe(gulp.dest('./'));
+gulp.task('bump', async function() {
+	gulp
+		.src('./package.json')
+		.pipe(
+			bump({
+				type: 'patch'
+			})
+		)
+		.pipe(gulp.dest('./'));
+
+	return true;
 });
 
-gulp.task('copy', function (cb) {
+gulp.task('copy', async function(cb) {
+	const pkgjson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 
-    const pkgjson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+	delete pkgjson.scripts;
 
-    delete pkgjson.scripts;
+	delete pkgjson.devDependencies;
 
-    delete pkgjson.devDependencies;
+	const filepath = './dist/package.json';
 
-    const filepath = './dist/package.json';
+	fs.writeFileSync(filepath, JSON.stringify(pkgjson, null, 2), 'utf-8');
 
-    fs.writeFileSync(filepath, JSON.stringify(pkgjson, null, 2), 'utf-8');
+	return true;
 });
 
-gulp.task('git-add', function (cb) {
-    exec('git add -A', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        cb(err);
-    });
+gulp.task('git-add', function(cb) {
+	exec('git add -A', function(err, stdout, stderr) {
+		console.log(stdout);
+		console.log(stderr);
+		cb(err);
+	});
 });
 
+gulp.task('git-commit', function(cb) {
+	var package = require('./package.json');
 
-gulp.task('git-commit', function (cb) {
-
-    var package = require('./package.json');
-
-    exec('git commit -m "Version ' + package.version + ' release."', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        cb(err);
-    });
+	exec('git commit -m "Version ' + package.version + ' release."', function(
+		err,
+		stdout,
+		stderr
+	) {
+		console.log(stdout);
+		console.log(stderr);
+		cb(err);
+	});
 });
 
-gulp.task('git-push', function (cb) {
-
-    exec('git push', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        cb(err);
-    });
+gulp.task('git-push', function(cb) {
+	exec('git push', function(err, stdout, stderr) {
+		console.log(stdout);
+		console.log(stderr);
+		cb(err);
+	});
 });
 
-gulp.task('publish', function (cb) {
-
-    exec('npm publish ./dist', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        cb(err);
-    });
+gulp.task('publish', function(cb) {
+	exec('npm publish ./dist', function(err, stdout, stderr) {
+		console.log(stdout);
+		console.log(stderr);
+		cb(err);
+	});
 });
-
